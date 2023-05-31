28f2ba5343aa62995415537a7051de3a8869885c
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-05-31 18:43:08 -0400

    import

--- TestExpendedor.java
+++ TestExpendedor.java
@@ -1,3 +1,6 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 class TestExpendedor{
     @Test
     public void testComprarBebida() throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
