c02c35eea7711ffc41b75b556accc0cf6032eecb
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-05-31 16:47:38 -0400

    Create Testcomprador.java

---
+++ Testcomprador.java
@@ -0,0 +1,16 @@
+class Testcomprador {
+    @Test
+    public void testCompraExitosa() throws PagoIncorrectoException {
+        // Configuración de prueba
+        Moneda m = new Moneda1000();  // Crea un objeto Moneda válido
+        int cual = Expendedor.COCA;  // Define el tipo de producto a comprar
+        Expendedor exp = new Expendedor(10, 500, 300);  // Crea un objeto Expendedor válido
+        // Crea una instancia de Comprador
+        Comprador comprador = new Comprador(m, cual, exp);
+
+        // Verificación de los resultados esperados
+        // Asegúrate de adaptar estas afirmaciones a los detalles específicos de tu implementación
+        assertEquals(500, comprador.cuantoVuelto());
+        assertEquals("cocacola", comprador.queSacaste());
+    }
+}
