de177e942d4ee05b92039a8e4100aa54d90622de
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-05-31 18:41:55 -0400

    se agregan mas test

--- Main.java
+++ Main.java
@@ -1,13 +1,32 @@
 package org.example;
 
-import java.util.ArrayList;
 
-public class Main {
-    public static void main(String[] args) throws PagoIncorrectoException {
-        // Crear una instancia de la clase de prueba
-        Testcomprador tester = new Testcomprador();
 
-        // Ejecutar el método de prueba
-        tester.testCompraExitosa();
+public static void main(String[] args) throws PagoIncorrectoException, PagoInsuficienteException, NoHayProductoException {
+        Testcomprador test1 = new Testcomprador();
+        test1.testCompraExitosa();
+
+        TestExpendedor test2= new TestExpendedor();
+        test2.testComprarBebida();
+        test2.testComprarDulce();
+        test2.testGetVuelto();
+
+        TestDeposito test3= new TestDeposito();
+        test3.testAddBebida();
+        test3.testAddDulce();
+        test3.testAddMoneda();
+
+        Moneda100Test test4= new Moneda100Test();
+        test4.getValorTest();
+
+        Moneda1000Test test5= new Moneda1000Test();
+        test5.getValor();
+
+        Moneda1500Test test6= new Moneda1500Test();
+        test6.getValor();
+
+        Moneda500Test test7= new Moneda500Test();
+        test7.getValor();
+        
     }
 }
