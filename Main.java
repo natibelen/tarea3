2257ba3399ebc32e1832d9604e2c8a6fca1a251c
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-05-31 16:49:38 -0400

    se prueba testcomprador

--- Main.java
+++ Main.java
@@ -3,58 +3,11 @@ package org.example;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PagoIncorrectoException {
+        // Crear una instancia de la clase de prueba
+        Testcomprador tester = new Testcomprador();
 
-        Expendedor exp = new Expendedor(2,500,300);
-        Moneda m = null;
-        Comprador c=null;
-        m = new Moneda1000();
-        c = new Comprador(m,exp.SPRITE,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda500();
-        c = new Comprador(m,exp.SPRITE,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.SPRITE,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.COCA,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1500();
-        c = new Comprador(m,exp.COCA,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.COCA,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.COCA,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda500();
-        c = new Comprador(m,exp.SNICKERS,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = null;
-        c = new Comprador(m,exp.SNICKERS,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,exp.SNICKERS,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.SNICKERS,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.SNICKERS,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda100();
-        c = new Comprador(m,exp.SUPER8,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda1000();
-        c = new Comprador(m,exp.SUPER8,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda500();
-        c = new Comprador(m,exp.SUPER8,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
-        m = new Moneda500();
-        c = new Comprador(m,exp.SUPER8,exp);
-        System.out.println(c.queSacaste()+", "+c.cuantoVuelto());
+        // Ejecutar el método de prueba
+        tester.testCompraExitosa();
     }
 }
