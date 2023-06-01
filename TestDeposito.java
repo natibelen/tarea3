ed97c35dc29810d27a28434e67638a4db88fee9f
natibelen <131008881+natibelen@users.noreply.github.com>
2023-05-31 23:00:38 -0400

    Update TestDeposito.java
    agrega tests de getBebida, getDulce y getMoneda. 
    revisa que sean null ya que esta función tiene como objetivo pretender sacar el producto del deposito

--- TestDeposito.java
+++ TestDeposito.java
@@ -41,5 +41,41 @@ class TestDeposito {
         deposito.addDulce(dulce);
         assertEquals(dulce, deposito.getDulce());
     }
+    @Test
+    void getBebida() {
+        Deposito deposito = new Deposito();
+        Bebida bebida = new CocaCola(1);
+
+        deposito.addBebida(bebida);
+        deposito.getBebida();
+
+        //debe asegurarse que no retorna una bebida, ya que la saca
+        assertEquals(null,deposito.getBebida());
+    }
+
+    @Test
+    void getDulce() {
+        Deposito deposito = new Deposito();
+        Dulce dulce = new Snickers(1);
+
+        deposito.addDulce(dulce);
+        deposito.getDulce();
+
+        //debe asegurarse que no retorna un dulce, ya que la saca
+        assertEquals(null,deposito.getDulce());
+    }
+
+    @Test
+    void getMoneda() {
+        Deposito deposito = new Deposito();
+        Moneda moneda = new Moneda1000();
+
+        deposito.addMoneda(moneda);
+        deposito.getMoneda();
+
+        //debe asegurarse que no retorna una moneda, ya que la saca
+        assertEquals(null,deposito.getMoneda());
+    }
+}
 
 }
